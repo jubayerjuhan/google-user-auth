8b8ab831f09c7bd6116681aa5150949f9cf63e9c
jubayerjuhan <davidjuhan23@gmail.com>
2021-07-26 15:40:38 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./Firebase-config";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignin = () => {
+    console.log('sign in clicked yey');
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, email, photoURL} = res.additionalUserInfo.profile;
+      console.log(res);
+      console.log(displayName, email, photoURL);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSignin} className>Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUCCd3ohAHXZtdXHdNAH9Gqid2dA3HCyk",
+    authDomain: "ema-john-simple-c4c43.firebaseapp.com",
+    projectId: "ema-john-simple-c4c43",
+    storageBucket: "ema-john-simple-c4c43.appspot.com",
+    messagingSenderId: "1075380726278",
+    appId: "1:1075380726278:web:53563bf4b5df7c8fbc3187"
+  };
+
+export default firebaseConfig;
